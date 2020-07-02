658b3a1a7565db9af81594730d896c5ea8f97296
Magnus <Renen@DESKTOP-88ILJBA>
2020-07-02 17:02:06 +0200

    ee

--- IOTManagementApp/Startup.cs
+++ IOTManagementApp/Startup.cs
@@ -28,9 +28,9 @@ namespace IOTManagementApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
+        //    services.AddDbContext<ApplicationDbContext>(options =>
+          //      options.UseSqlServer(
+           //         Configuration.GetConnectionString("DefaultConnection")));
             services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = false)
                 .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
